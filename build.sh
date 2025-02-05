ed302a95fc6460b05ec1091524caf72915010829
Carlana <mail@carlana.net>
2025-02-04 23:26:13 -0500

    build.sh: Copy static files into public

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -eu -o pipefail
+set -eux -o pipefail
 
 # Get the directory that this script file is in
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "$THIS_DIR"
 
+shopt -s extglob # Enable negative globbing
+cp static/!(*.go) public
 go build -o functions/gmailsig .
