6d1c87f976168ab1a00190eeb39c87f5281d09e3
Carlana <mail@carlana.net>
2025-02-08 18:47:21 -0500

    Redirect deploy preview logins

--- webapp/auth.go
+++ webapp/auth.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/carlmjohnson/resperr"
 	"golang.org/x/oauth2"
@@ -39,7 +40,7 @@ func (app *appEnv) googleConfig(scopes ...string) *oauth2.Config {
 func (app *appEnv) authRedirect(w http.ResponseWriter, r *http.Request, scopes ...string) {
 	app.setCookie(w, redirectURLCookie, r.URL)
 
-	stateToken := rand.Text()
+	stateToken := r.Host + "|" + rand.Text()
 	app.setCookie(w, stateCookie, stateToken)
 
 	app.setCookie(w, scopesCookie, scopes)
@@ -52,6 +53,16 @@ func (app *appEnv) authRedirect(w http.ResponseWriter, r *http.Request, scopes .
 }
 
 func (app *appEnv) authCallback(w http.ResponseWriter, r *http.Request) {
+	// Redirect if necessary for deploy previews
+	callbackState := r.FormValue("state")
+	host, _, _ := strings.Cut(callbackState, "|")
+	if host != r.Host && strings.HasSuffix(host, "--gmailsig.netlify.app") {
+		u := *r.URL
+		u.Host = host
+		logger.Printf("redirecting log in to %q", host)
+		http.Redirect(w, r, u.String(), http.StatusSeeOther)
+		return
+	}
 	var state string
 	if !app.getCookie(r, stateCookie, &state) {
 		app.replyHTMLErr(w, r, resperr.New(http.StatusUnauthorized, "no saved state"))
@@ -73,7 +84,7 @@ func (app *appEnv) authCallback(w http.ResponseWriter, r *http.Request) {
 	}
 	app.deleteCookie(w, scopesCookie)
 
-	if callbackState := r.FormValue("state"); state != callbackState {
+	if state != callbackState {
 		app.replyHTMLErr(w, r, resperr.New(
 			http.StatusBadRequest,
 			"token %q != %q",
